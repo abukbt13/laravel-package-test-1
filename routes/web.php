c2312e98fe16a26955f2451ee25ebcccaafcb4c0
abukbt13 <abukbt13@gmail.com>
2024-09-17 09:10:34 +0300

    test package

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ($length = 10) {
+    return bin2hex(random_bytes($length));
 });
